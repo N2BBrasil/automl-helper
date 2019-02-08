79aad5a5ac0cfa3e2d4de11bde02833f598253ea
Samuel Heinrichs <ti.samuelh@gmail.com>
2019-02-08 14:35:56 -0200

    docs and examples

--- automlhelper/__init__.py
+++ automlhelper/__init__.py
@@ -2,6 +2,13 @@ from automlhelper.storageio import StorageIO
 from automlhelper.automl import AutoML
 
 class AutoMLHelper:
+    """AutoML Helper
+
+    Args:
+        bucket_name (int): The name of the bucket utilized in AutoML e.g.: my-bucket-vcm
+        csv_path (str): The path where yours CSVs with reference of images and classes of your images. e.g.: csv/
+        project_name (str): Your Google Cloud project 
+    """
 
     def __init__(self, bucket_name, csv_path, project_name):
         self.bucket_name = bucket_name
@@ -13,6 +20,14 @@ class AutoMLHelper:
         self.tags = False
 
     def tag_performance(self):
+        """Returns tag `display_name` and performance object.
+
+        Warning: This connection between tag display_name and the performance object is not returned by Google`s API, this method utilizes an alternative way that can stop working at any moment.
+
+        Returns:
+            list: (display_name, performance_object)
+        """
+
         if self.performance == False:
             self.performance = self.automl.get_tag_evaluation()
 
@@ -22,12 +37,32 @@ class AutoMLHelper:
         return zip(self.tags, self.performance)
     
     def tag_and_performance(self, key='base_au_prc'):
+        """Returns tag `display_name` and performance metric.
+
+        Args:
+            key (str): performance metric of performance object defaults: 'base_au_prc'
+
+        Returns:
+            list: (display_name, performance_metric)
+        """
+
         values = []
         for t,p in self.tag_performance():
             values.append((t, getattr(p.classification_evaluation_metrics, key)))
         return values
 
     def best_performing(self, key='base_au_prc', limit=0, reverse=True): 
+        """Returns best or worst tags by performance metric.
+
+        Args:
+            key (str): performance metric of the performance object defaults: 'base_au_prc'
+            limit (int): max number of results
+            reverse (bool): True for best performing, False for worst performing
+
+        Returns:
+            list: (display_name, performance_metric)
+        """
+        
         values = sorted(self.tag_and_performance(key), key=lambda _: _[1], reverse=reverse)
         
         if limit > 0:
@@ -36,16 +71,33 @@ class AutoMLHelper:
             return values
 
     def get_tags(self):
+        """Returns all tags in the last CSV.
+
+        Returns:
+            list: display_name
+        """
+
         lines = self.storageio.get_csv_lines()
         tags = []
         for l in lines:
             for t in l.split(','):
-                if 'gs://' not in t and t not in tags + ['TRAIN','TEST','VALIDATION']:
+                if 'gs://' not in t and t not in tags + [
+                    'TRAIN','TEST','VALIDATION']:
                     tags.append(t)
 
         return sorted(tags)
 
     def get_examples_of(self, tag_name, limit=10):
+        """Returns examples of tag_name in the last CSV.
+
+        Args:
+            tag_name (str): tag display_name
+            limit (int): max number of results
+
+        Returns:
+            list: files marked with that tag
+        """
+
         csv_lines = self.storageio.get_csv_lines()
         files = []
 
@@ -61,6 +113,15 @@ class AutoMLHelper:
         return files
 
     def predict_gs(self, gs_file):
+        """Returns AutoML predictions on gs_file.
+
+        Args:
+            gs_file (str): image in the gs bucket e.g.: gs://my-bucket-name/image.jpg
+
+        Returns:
+            list: (`display_name`, classification score)
+        """       
+
         return self.automl.get_predictions(
             self.storageio.get_base64_from_gs(gs_file)
         )
--- example.py
+++ example.py
@@ -1,16 +1,21 @@
 import os
 from automlhelper import AutoMLHelper
 
+#Initialize Object
 auto = AutoMLHelper(
     bucket_name=os.environ['AUTOML_BUCKET_NAME'],
     csv_path=os.environ['AUTOML_CSV_PATH'],
     project_name=os.environ['AUTML_PROJECT_NAME'])
 
-best_performing = auto.best_performing(limit=10, reverse=False)
-
+#Return 10 best performing tags
+best_performing = auto.best_performing(limit=10, reverse=True)
 print(best_performing)
-print(auto.best_performing(limit=10, reverse=True))
 
-link = auto.get_examples_of(best_performing[0][0])[0]
-print(link)
+#Return 10 worst performing tags
+worst_performing = auto.best_performing(limit=10, reverse=False)
+print(worst_performing)
+
+#Get prediction results
+gs_file = auto.get_examples_of(best_performing[0][0])[0]
+print(gs_file)
 print(auto.predict_gs(link))
